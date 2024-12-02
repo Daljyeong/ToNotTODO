0d91955f65de75c874086e8d96739d5e990f5bf9
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-12-03 02:19:36 +0900

    feat: 화면 이동 구현 완료

--- app/src/main/java/com/example/tonottodo/SeesawRecordFragment.kt
+++ app/src/main/java/com/example/tonottodo/SeesawRecordFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.tonottodo.databinding.FragmentSeesawRecordBinding
 
 class SeesawRecordFragment : Fragment() {
@@ -16,6 +17,10 @@ class SeesawRecordFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentSeesawRecordBinding.inflate(layoutInflater)
+
+        binding.clSeesaw1.setOnClickListener {
+            findNavController().navigate(R.id.action_seesaw_record_to_goal_detail)
+        }
 
         return binding.root
     }
